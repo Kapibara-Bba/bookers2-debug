ddc6e4fbb62e95b49f68c55c584d2bd0aee98735
EC2 Default User <kapibara.bba0924@gmail.com>
2020-10-17 08:03:44 +0000

    [Add]bookers2-debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  #before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
@@ -14,8 +15,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end
-  
+
   def create
     @user = User.new(user_params)
     @users = User.all
@@ -24,10 +28,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
@@ -36,10 +41,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
-  #def ensure_correct_user
-   # @user = User.find(params[:id])
-    #if @user == current_user
-     # redirect_to user_path(current_user)
-    #end
-  #end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    if @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
